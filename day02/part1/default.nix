b333d1bc2a93a88815399d865fa99ee9df6baefa
Matthew Toohey <contact@mtoohey.com>
2022-12-02 08:47:02 -0500

    feat: day02

---
+++ day02/part1/default.nix
@@ -0,0 +1,29 @@
+let
+  inherit (builtins) add elemAt filter foldl' isString readFile split;
+  splitString = sep: s: filter isString (split sep s);
+  sum = foldl' add 0;
+
+  shapeScore = {
+    X = 1;
+    Y = 2;
+    Z = 3;
+  };
+  winScore = {
+    "A X" = 3;
+    "A Y" = 6;
+    "A Z" = 0;
+
+    "B X" = 0;
+    "B Y" = 3;
+    "B Z" = 6;
+
+    "C X" = 6;
+    "C Y" = 0;
+    "C Z" = 3;
+  };
+
+  input = readFile ../input;
+  scores = map (l: winScore.${l} + shapeScore."${(elemAt (splitString " " l) 1)}") (splitString "\n" input);
+  answer = sum scores;
+in
+builtins.toString answer
---
+++ day02/part2/default.nix
@@ -0,0 +1,29 @@
+let
+  inherit (builtins) add elemAt filter foldl' isString readFile split;
+  splitString = sep: s: filter isString (split sep s);
+  sum = foldl' add 0;
+
+  winScore = {
+    X = 0;
+    Y = 3;
+    Z = 6;
+  };
+  shapeScore = {
+    "A X" = 3;
+    "A Y" = 1;
+    "A Z" = 2;
+
+    "B X" = 1;
+    "B Y" = 2;
+    "B Z" = 3;
+
+    "C X" = 2;
+    "C Y" = 3;
+    "C Z" = 1;
+  };
+
+  input = readFile ../input;
+  scores = map (l: shapeScore.${l} + winScore."${(elemAt (splitString " " l) 1)}") (splitString "\n" input);
+  answer = sum scores;
+in
+builtins.toString answer
