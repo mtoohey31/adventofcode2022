99f3d5c6fbf3f0b925a73e1e2984348cb0a26d2d
Matthew Toohey <contact@mtoohey.com>
2022-12-05 01:08:53 -0500

    feat: day05/nix

---
+++ day05/nix/part1/default.nix
@@ -0,0 +1,54 @@
+let
+  inherit (builtins) concatStringsSep elemAt head filter foldl' isList length
+    split stringLength substring tail;
+  inherit (import ../../../nix/lib.nix) drop splitString readFileTrimmed rev
+    seq take toInt updateList;
+
+  toMove = s:
+    let
+      matchLists = filter isList
+        (split "move ([[:digit:]]+) from ([[:digit:]]+) to ([[:digit:]]+)" s);
+      matches = assert length matchLists == 1; head matchLists;
+    in
+    assert length matches == 3; {
+      count = toInt (head matches);
+      from = toInt (head (tail matches));
+      to = toInt (head (tail (tail matches)));
+    };
+  toStacks = s:
+    let lines = splitString "\n" s; in
+    map
+      (i: filter (c: c != " ")
+        (map
+          (j: substring ((i * 4) + 1) 1 (elemAt lines j))
+          (seq 0 ((length lines) - 2)))
+      )
+      (seq 0 ((((stringLength (head lines)) + 1) / 4) - 1));
+
+  input = readFileTrimmed ../../input;
+  parts = splitString "\n\n" input;
+  startingStacksString = assert length parts == 2; head parts;
+  procedureString = head (tail parts);
+
+  startingStacks = toStacks startingStacksString;
+  moves = map toMove (splitString "\n" procedureString);
+
+  finalStacks = foldl'
+    (stacks: move:
+      let
+        fromStack = elemAt stacks (move.from - 1);
+        fromAfter = drop move.count fromStack;
+        toStack = elemAt stacks (move.to - 1);
+        moved = take move.count fromStack;
+        toAfter = (rev moved) ++ toStack;
+
+        stacksWithNewFrom = updateList stacks fromAfter (move.from - 1);
+      in
+      updateList stacksWithNewFrom toAfter (move.to - 1)
+    )
+    startingStacks
+    moves;
+
+  answer = concatStringsSep "" (map head finalStacks);
+in
+builtins.toString answer
--- nix/lib.nix
+++ nix/lib.nix
@@ -1,9 +1,11 @@
 let
-  inherit (builtins) add filter foldl' fromJSON head isString match readFile
-    split stringLength substring;
+  inherit (builtins) add filter foldl' fromJSON head isString length match
+    readFile split stringLength substring tail;
 in
 rec {
   attrOf = s: a: s.${a};
+
+  drop = n: l: if n > 0 then (drop (n - 1) (tail l)) else l;
 
   max = foldl' (max: n: if max == null || max < n then n else max) null;
 
@@ -18,6 +20,16 @@ rec {
     in
     substring 0 (bodyLength - trailingLength) body;
 
+  repeat = e: n: if n == 0 then [ ] else [ e ] ++ (repeat e (n - 1));
+
+  rev = l: if length l > 0 then (rev (tail l)) ++ [ (head l) ] else [ ];
+
+  # inclusive
+  seq = min: max: if min <= max then [ min ] ++ (seq (min + 1) max) else [ ];
+
+  # inclusive
+  take = n: l: if n == 0 then [ ] else [ (head l) ] ++ (take (n - 1) (tail l));
+
   splitString = sep: s: filter isString (split sep s);
 
   _stringToSet = s: a: if stringLength s == 0 then a else
@@ -31,4 +43,9 @@ rec {
   sum = foldl' add 0;
 
   toInt = fromJSON;
+
+  _updateList = l: v: i:
+    if i == 0 then [ v ] ++ (tail l)
+    else [ (head l) ] ++ (_updateList (tail l) v (i - 1));
+  updateList = l: v: i: assert i < length l; _updateList l v i;
 }
