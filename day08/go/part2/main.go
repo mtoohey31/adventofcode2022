e1a34cd65b5e5ffdf2eb19da2608c472d8b9c26b
Matthew Toohey <contact@mtoohey.com>
2022-12-08 13:37:19 -0500

    perf(day08/go/part2): dynamic solution

--- day08/go/part2/main.go
+++ day08/go/part2/main.go
@@ -7,6 +7,11 @@ import (
 	"strings"
 )
 
+type tree struct {
+	height                                    int
+	upCount, rightCount, downCount, leftCount int
+}
+
 func main() {
 	input, err := os.ReadFile("../../input")
 	if err != nil {
@@ -17,62 +22,79 @@ func main() {
 	lines = lines[:len(lines)-1]
 	size := len(lines)
 
-	forest := make([][]int, size)
+	forest := make([][]*tree, size)
 	for i, line := range lines {
 		if len(line) != size {
 			panic("unexpected line length")
 		}
-		forest[i] = make([]int, size)
+		forest[i] = make([]*tree, size)
 		for j, char := range line {
-			forest[i][j], err = strconv.Atoi(string(char))
+			forest[i][j] = new(tree)
+			forest[i][j].height, err = strconv.Atoi(string(char))
 			if err != nil {
 				panic(err)
 			}
 		}
 	}
 
-	maxScore := 0
-
-	for i, row := range forest {
-		if i == 0 || i == size-1 {
-			// up/down distance will be zero; so will score
-			continue
+	// rows
+	for _, row := range forest {
+		// left to right
+		lrHeightPos := [10]int{}
+		for i, tree := range row {
+			tree.leftCount = lrHeightPos[tree.height] - i
+			for j := 0; j <= tree.height; j++ {
+				lrHeightPos[j] = i
+			}
 		}
 
-		for j, tree := range row {
-			if j == 0 || j == size-1 {
-				// left/right distance will be zero; so will score
-				continue
+		// right to left
+		rlHeightPos := [10]int{}
+		for i := 0; i < 10; i++ {
+			rlHeightPos[i] = size - 1
+		}
+		for i := size - 1; i >= 0; i-- {
+			tree := row[i]
+			tree.rightCount = rlHeightPos[tree.height] - i
+			for j := 0; j <= tree.height; j++ {
+				rlHeightPos[j] = i
 			}
+		}
+	}
 
-			upCount, rightCount, downCount, leftCount := 1, 1, 1, 1
-
-			// up
-			for k := i - 1; k > 0 && forest[k][j] < tree; k-- {
-				upCount++
+	// columns
+	for i := 0; i < size; i++ {
+		// top to bottom
+		tbHeightPos := [10]int{}
+		for j := 0; j < size; j++ {
+			tree := forest[j][i]
+			tree.upCount = tbHeightPos[tree.height] - j
+			for k := 0; k <= tree.height; k++ {
+				tbHeightPos[k] = j
 			}
+		}
 
-			// right
-			for k := j + 1; k < size-1 && forest[i][k] < tree; k++ {
-				rightCount++
+		btHeightPos := [10]int{}
+		for i := 0; i < 10; i++ {
+			btHeightPos[i] = size - 1
+		}
+		for j := size - 1; j >= 0; j-- {
+			tree := forest[j][i]
+			tree.downCount = btHeightPos[tree.height] - j
+			for k := 0; k <= tree.height; k++ {
+				btHeightPos[k] = j
 			}
+		}
+	}
 
-			// down
-			for k := i + 1; k < size-1 && forest[k][j] < tree; k++ {
-				downCount++
-			}
-
-			// left
-			for k := j - 1; k > 0 && forest[i][k] < tree; k-- {
-				leftCount++
-			}
-
-			score := upCount * rightCount * downCount * leftCount
+	maxScore := 0
+	for _, row := range forest {
+		for _, tree := range row {
+			score := tree.upCount * tree.rightCount * tree.downCount * tree.leftCount
 			if score > maxScore {
 				maxScore = score
 			}
 		}
 	}
-
 	fmt.Print(maxScore)
 }
